40a5069b4d7d965b028d25ecbb5391ff67eb59d6
cwmle <cwmlee00@gmail.com>
2025-01-30 20:51:54 -0600

    setting up actions

--- src/main.tsx
+++ src/main.tsx
@@ -11,9 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <BrowserRouter>
             <WebSocketProvider>
                 <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/lobby/:gameId" element={<Lobby />} />
-                    <Route path="/game/:gameId" element={<Game />} />
+                    <Route path="/AI-Jeopardy/" element={<MainPage />} />
+                    <Route path="/AI-Jeopardy/lobby/:gameId" element={<Lobby />} />
+                    <Route path="/AI-Jeopardy/game/:gameId" element={<Game />} />
                     {/* Add other routes for Game, etc., here */}
                 </Routes>
             </WebSocketProvider>
